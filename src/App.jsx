726071be4c79210ed3255fa9ab7e4faed8f5dce2
shadow6444 <shadowgarden644@gmail.com>
2024-09-27 20:00:25 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import fetchAPI from "./fetchAPI";
 
-function App() {
-
+const App = () => {
+  console.log(fetchAPI("all"));
   return (
     <>
-
+      <Navbar />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
