29be221af39935a29490b4a5c763e754d682380b
shadow6444 <shadowgarden644@gmail.com>
2024-09-29 00:32:40 +0500

    Update Recipe.jsx

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,12 +1,10 @@
 import "./Recipe.css";
 
-const Recipe = ({ image, title }) => {
+const Recipe = ({ image, title, otherData, onSelect }) => {
   return (
-    <div className="recipe">
-      <a href="#">
-        <img src={image} alt="recipe img" className="recipe-img"/>
-        <div className="recipe-title">{title.split('(')[0]}</div>
-      </a>
+    <div className="recipe" onClick={()=>onSelect(otherData)}>
+      <img src={image} alt="recipe img" className="recipe-img" />
+      <div className="recipe-title">{title.split("(")[0]}</div>
     </div>
   );
 };
