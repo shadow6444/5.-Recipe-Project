3082e77b826b903f9ebd66372c97b6e3ae87b469
shadow6444 <shadowgarden644@gmail.com>
2024-09-29 19:25:28 +0500

    Added Click Handler

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,8 +1,8 @@
 import "./Recipe.css";
 
-const Recipe = ({ image, title, otherData, onSelect }) => {
+const Recipe = ({ image, title, otherData, onSelect, prepData }) => {
   return (
-    <div className="recipe" onClick={()=>onSelect(otherData)}>
+    <div className="recipe" onClick={() => onSelect(otherData, prepData)}>
       <img src={image} alt="recipe img" className="recipe-img" />
       <div className="recipe-title">{title.split("(")[0]}</div>
     </div>
