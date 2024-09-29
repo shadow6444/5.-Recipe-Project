1c2ee63d24cf489825a5f77ddb17d277e2195fae
shadow6444 <shadowgarden644@gmail.com>
2024-09-29 19:25:38 +0500

    Create PREP.js

---
+++ src/PREP.js
@@ -0,0 +1,32 @@
+const PREP = [
+  {
+    id: 1,
+    info: `Osso buco is a classic Italian dish originating in Milan. The name "osso buco" (in Milanese dialect "Oss Buss") literally means "bone with a hole", referring to the marrow hole in the center of the cross-cut veal shank used in this dish. The Osso Buco recipe, although it requires a long cooking time, is not a difficult dish to make. What makes it truly special is its tenderness and unique flavor.`,
+    prepTime: "20 mins.",
+    cookTime: "2 hrs.",
+    servings: "4 servings",
+  },
+  {
+    id: 2,
+    info: `Callaloo is a delicious green that's commonly referred to as Jamaican spinach. Its true name is slender amaranth (the leaves of the amaranth plant) and can even be found growing wild in the United States. Like spinach, sautéed callaloo is a versatile dish. Most often mixed with rehydrated salted cod fish and eaten for breakfast with boiled green bananas, boiled yams, dumplings, and of course, fried plantains, callaloo can be mixed with rice to make a delicious side dish. Or serve callaloo with jerk chicken and rice and peas. `,
+    prepTime: "10 mins.",
+    cookTime: "10 mins.",
+    servings: "4-6 servings",
+  },
+  {
+    id: 3,
+    info: `Spaghetti alla carbonara. Luscious and wonderfully indulgent, pasta carbonara takes as long to make as it does to cook the pasta. The ingredients are simple—just spaghetti (or another long pasta), and the carbonara is made with pancetta or bacon, eggs, Parmesan, a little olive oil, salt and pepper. The silky carbonara sauce is created when the beaten eggs are tossed with the hot pasta and a little fat from the pancetta or bacon. `,
+    prepTime: "5 mins.",
+    cookTime: "15 mins.",
+    servings: "4 servings",
+  },
+  {
+    id: 4,
+    info: `Bitterballen are a classic Dutch snack. These deep-fried meatballs are made with a savoury mixture of beef, onions, and spices, coated in a crispy breadcrumb crust. Whether you're serving them as an appetiser, a main course, or a late-night snack, bitterballen are a surefire hit.`,
+    prepTime: "2 hrs.",
+    cookTime: "10 mins.",
+    servings: "8 servings",
+  },
+];
+
+export default PREP;
