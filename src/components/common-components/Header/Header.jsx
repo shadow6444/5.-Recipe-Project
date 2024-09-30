dec4f918031300202d68b78cb34118f1fe54790a
shadow6444 <shadowgarden644@gmail.com>
2024-09-30 12:49:00 +0500

    Create Header.jsx

---
+++ src/components/common-components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="heading text-center">
+        <h3>
+          <span className="heading-style1">simple recipes made for</span>
+          <span className="heading-style2 font-thin">
+            real, actual, everyday life.
+          </span>
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default Header;
